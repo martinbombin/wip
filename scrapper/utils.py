4cbf1a184f4176adcde56784c98360321451b245
martinbombin <martn.b.o@gmail.com>
2025-02-28 15:05:50 +0100

    feat: add timer decorators

--- scrapper/utils.py
+++ scrapper/utils.py
@@ -5,6 +5,43 @@ extract_dl_text(dl_tag: BeautifulSoup) -> str:
 Extract and concatenate text from <dl> tags, including <dt> and <dd> items.
 """
 
+import logging
+import time
+
+logging.basicConfig(level=logging.INFO)
+
+
+def async_timer(func):
+    async def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = await func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        logging.info(
+            "Function '%s' executed in %.4f seconds",
+            func.__name__,
+            elapsed_time,
+        )
+        return result
+
+    return wrapper
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        logging.info(
+            "Function '%s' executed in %.4f seconds",
+            func.__name__,
+            elapsed_time,
+        )
+        return result
+
+    return wrapper
+
 
 def extract_dl_text(dl_tag) -> str:
     """Extract and concatenate text from <dl> tags, including <dt> and <dd> items.

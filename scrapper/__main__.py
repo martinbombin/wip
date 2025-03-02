0d459b2f2e5367882c234244a17e1e9872b7db13
martinbombin <martn.b.o@gmail.com>
2025-03-03 00:11:14 +0100

    chore: remove already moved methods

--- scrapper/__main__.py
+++ scrapper/__main__.py
@@ -1,53 +1,13 @@
 import asyncio
 import logging
-from concurrent.futures import ThreadPoolExecutor
 
 from . import utils
 from .crawler import Crawler
-
-# import schedule
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
-
-
-def set_page_content(page):
-    """Set content for a page using a threadpool executor."""
-    try:
-        browser = init_browser()
-        try:
-            page.set_content(browser)
-        except Exception as e:
-            logging.exception(
-                "Error processing page %s: %s",
-                page.url,
-                e,
-            )
-        finally:
-            browser.quit()
-    except Exception as e:
-        logging.exception(
-            "Error with browser initialization for page %s: %s",
-            page.url,
-            e,
-        )
-
-
-async def scrap_categories(max_workers: int = 5):
-    crawler = Crawler()
-    # crawler.set_categories(browser)
-    # crawler.save_categories("categories.json")
-    crawler.load_categories("categories.json")
-
-    pages = [page for cat in crawler.categories for page in cat.pages]
-    max_workers = min(5, len(pages))
-
-    with ThreadPoolExecutor(max_workers=max_workers) as executor:
-        list(executor.map(set_page_content, pages))
-
-    crawler.save_categories("scrapped_test.json")
 
 
 async def main():
